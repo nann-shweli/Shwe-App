52d2a3a50c60fa7cb799c062539499f3bbbde1b7
NANNSHWELI <shweli@ayainnovation.com>
2025-06-10 10:19:26 +0630

    cupdate app icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
+
 import AppNavigation from './navigation/AppNavigation';
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      await RNBootSplash.hide({fade: true});
+    };
+    init();
+  }, []);
+
   return <AppNavigation />;
 };
 
